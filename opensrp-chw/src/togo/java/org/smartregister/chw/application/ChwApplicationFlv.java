3aee49ac32d1044a90ff09069f31ff6eead8f4d3
issyzac <issyzac.iz@gmail.com>
2022-12-13 10:19:38 +0300

    :bucket: refactor

--- opensrp-chw/src/togo/java/org/smartregister/chw/application/ChwApplicationFlv.java
+++
@@ -1,8 +0,0 @@
-package org.smartregister.chw.application;
-
-public class ChwApplicationFlv extends DefaultChwApplicationFlv {
-    @Override
-    public boolean showChildrenUnderFiveAndGirlsAgeNineToEleven() {
-        return true;
-    }
-}
